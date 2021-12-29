29b9fc68989dba3ee4cb9aa21a511a5459180755
NMTuan <nmtuan@qq.com>
2021-12-29 20:34:52 +0800

    调整api的export

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -2,22 +2,18 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2021-12-28 21:21:16
- * @LastEditTime: 2021-12-29 06:51:57
+ * @LastEditTime: 2021-12-29 20:18:44
  * @LastEditors: NMTuan
  * @Description: api插件
  * @FilePath: \sy_bookmarks\src\plugins\api.js
  */
 
-import fetch from '@/utils/fetch'
 import api from '@/utils/api'
 
 const plugin = {}
 
 plugin.install = function (Vue) {
-    Vue.prototype.api = Object.keys(api).reduce((total, key) => {
-        total[key] = (params) => fetch(api[key], params)
-        return total
-    }, {})
+    Vue.prototype.api = api
 }
 
 export default plugin
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,15 +2,16 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2021-12-28 15:19:15
- * @LastEditTime: 2021-12-29 07:50:29
+ * @LastEditTime: 2021-12-29 20:26:38
  * @LastEditors: NMTuan
  * @Description: api
  * @FilePath: \sy_bookmarks\src\utils\api.js
  */
 
-// https://github.com/siyuan-note/siyuan/blob/master/API.md
+import fetch from '@/utils/fetch'
 
-export default {
+// https://github.com/siyuan-note/siyuan/blob/master/API.md
+const api = {
     // 列出笔记本
     lsNotebooks: '/api/notebook/lsNotebooks',
     // 打开笔记本
@@ -54,11 +55,18 @@ export default {
 
     // 导出 Markdown 文本
     exportMdContent: '/api/export/exportMdContent',
-    
+
     // 获取启动进度
     bootProgress: '/api/system/bootProgress',
     // 获取系统版本
     version: '/api/system/version',
     // 获取系统当前时间
     currentTime: '/api/system/currentTime'
+
 }
+
+
+export default Object.keys(api).reduce((total, key) => {
+    total[key] = (params) => fetch(api[key], params)
+    return total
+}, {})
