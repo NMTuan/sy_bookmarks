c292c6b87a65f1ff95c304d2eccc1888b23dd2bd
NMTuan <nmtuan@qq.com>
2022-01-01 16:48:28 +0800

    适配文件夹

--- src/utils/handler.js
+++ src/utils/handler.js
@@ -2,19 +2,30 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2021-12-31 08:59:21
- * @LastEditTime: 2022-01-01 12:58:30
+ * @LastEditTime: 2022-01-01 16:39:45
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \sy_bookmarks\src\utils\handler.js
  */
 
 // url转siyuan存储路径
-export const url2path = function (url, path = '/') {
-    const reg = /^.*?:\/\/(.*?)\/?$/ig // 过滤掉协议头和最后的/
-    if (path.indexOf('/') !== 0) {
-        path = '/' + path
+export const url2path = function ({
+    url,
+    title,
+    parentId
+}) {
+    let path = '/';
+    if (parentId === '1') {
+        path += '书签栏/'
     }
-    return path + url.replace(reg, '$1')
+    if (parentId === '2') {
+        path += '其它书签/'
+    } else {
+        // TODO 根据parentId找到路径
+    }
+    const reg = /^.*?:\/\/(.*?)\/?$/ig // 过滤掉协议头和最后的/
+    // 有url，则用url做路径，没有则用title（文件夹）
+    return path + (url ? url.replace(reg, '$1') : title)
 }
 
 export const url2md = function (url, title = '') {
