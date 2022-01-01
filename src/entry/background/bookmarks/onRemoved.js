b709bc3100d89c7ec05ce1b0bf2c1a1409166d90
NMTuan <nmtuan@qq.com>
2022-01-01 14:22:42 +0800

    重写

--- src/entry/background/bookmarks/onRemoved.js
+++ src/entry/background/bookmarks/onRemoved.js
@@ -2,77 +2,54 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2021-12-30 15:04:06
- * @LastEditTime: 2021-12-31 15:53:11
+ * @LastEditTime: 2022-01-01 12:59:45
  * @LastEditors: NMTuan
  * @Description: 移除
  * @FilePath: \sy_bookmarks\src\entry\background\bookmarks\onRemoved.js
  */
 
 import api from '@/utils/api'
+import {
+    sleep
+} from '@/utils/handler'
 
-// 删除， bookmarkId， 重试次数
+// 找文档，新插入的可能无法立即找到
 // 由于新插入思源的数据需要等待一下才能查到，所以这里做了延时重试。
-const remove = (id, maxTime = 0) => {
-    if (maxTime === 0) {
-        return
-    }
-    maxTime--
-    api.sql({
+const findDoc = ({
+    id,
+    maxTime = 0
+}) => {
+    return api.sql({
             'stmt': `SELECT * FROM blocks WHERE ial LIKE '%custom-bookMark-id=\"${id}\"%'  LIMIT 1`
         })
-        .then(res => {
-            // {
-            //     "code": 0,
-            //     "msg": "",
-            //     "data": [{
-            //         "alias": "",
-            //         "box": "20211227104254-a0gvhbc",
-            //         "content": "extensions-zp4tqre",
-            //         "created": "20211231101040",
-            //         "hash": "62b7551",
-            //         "hpath": "/extensions-zp4tqre",
-            //         "ial": "{: id=\"20211231101040-6gv0bae\" title=\"extensions-zp4tqre\" updated=\"20211231101040\" custom-url=\"chrome://extensions/\" custom-title=\"扩展程序\" custom-dateAdded=\"1640916744134\" custom-type=\"bookmark\" custom-id=\"1376\"}",
-            //         "id": "20211231101040-6gv0bae",
-            //         "length": 0,
-            //         "markdown": "",
-            //         "memo": "",
-            //         "name": "",
-            //         "parent_id": "",
-            //         "path": "/20211231101040-6gv0bae.sy",
-            //         "root_id": "20211231101040-6gv0bae",
-            //         "sort": 0,
-            //         "subtype": "",
-            //         "type": "d",
-            //         "updated": "20211231101040"
-            //     }]
-            // }
-            console.log('remove', res, id)
-            if (res.length === 0) {
-                setTimeout(() => {
-                    remove(id, maxTime)
-                }, 1000)
-                return
+        .then(docs => {
+            //没找到，重试
+            if (docs.length === 0 && maxTime > 0) {
+                maxTime--
+                return sleep(findDoc, {
+                    id,
+                    maxTime
+                })
             }
-            api.renameDoc({
-                notebook: res[0].box,
-                path: res[0].path,
-                title: `[已删除]${res[0].content}`
-            })
+            return docs
         })
 }
 
-//     removeInfo = {
-//         index: 8,
-//         node: {
-//             dateAdded: 1640653826094,
-//             id: "1343",
-//             title: "百度翻译-200种语言互译、沟通全世界！",
-//             url: "https://fanyi.baidu.com/",
-//         },
+export default async function (id, removeInfo) {
+    const docs = await findDoc({
+        id,
+        maxTime: 10
+    })
+    if (docs.length === 0) {
+        return
+    }
 
-//         parentId: "1"
-//     }
-
-chrome.bookmarks.onRemoved.addListener(function (id, removeInfo) {
-    remove(id, 10)
-});
+    // 如果找到，则标记为删除
+    if (Array.isArray(docs) && docs[0].box) {
+        api.renameDoc({
+            notebook: docs[0].box,
+            path: docs[0].path,
+            title: `[已删除]${docs[0].content}`
+        })
+    }
+}
